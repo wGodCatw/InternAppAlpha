9d90e1001bef68fe59c18c97ea48a615e4b42bee
Ruslan <ruslan.yakubov05@gmail.com>
2023-12-28 13:35:34 +0200

    Google Sign in

---
+++ app/src/main/java/com/example/internapp/FirstFragment.java
@@ -0,0 +1,16 @@
+package com.example.internapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FirstFragment extends Fragment {
+    public FirstFragment() {
+        super(R.layout.fragment_first);
+    }
+}
---
+++ app/src/main/java/com/example/internapp/SecondFragment.java
@@ -0,0 +1,15 @@
+package com.example.internapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SecondFragment extends Fragment {
+    public SecondFragment() {
+        super(R.layout.fragment_second);
+    }
+}
