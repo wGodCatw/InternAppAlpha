09a8004b0712d1dd856dbace2974c5302854a804
Ruslan <ruslan.yakubov05@gmail.com>
2024-01-22 00:06:05 +0200

    Chip removal

--- app/src/main/java/com/example/internapp/SearchActivity.java
+++ app/src/main/java/com/example/internapp/SearchActivity.java
@@ -1,11 +1,10 @@
 package com.example.internapp;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.ViewCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -15,24 +14,44 @@ import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
+    public static TextView filtersTxt;
+    public static ArrayList<String> filters;
+    private static ChipGroup chipGroup;
     private RecyclerView UniversitiesRecView;
     private RecyclerView FacultiesRecView;
-    private static ChipGroup chipGroup;
+
+    public static void createChip(String text, View view) {
+        Chip chip = (Chip) LayoutInflater.from(view.getContext()).inflate(R.layout.chip_layout, null);
+        chip.setText(text);
+        chip.setId(ViewCompat.generateViewId());
+        chip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chipGroup.removeView(v);
+                filters.remove(text);
+                filtersTxt.setText("Filters: " + filters.toString());
+            }
+        });
+        chipGroup.addView(chip);
+        filters.add(text);
+        filtersTxt.setText("Filters: " + filters.toString());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        filtersTxt = findViewById(R.id.filtersTxt);
         FacultiesRecView = findViewById(R.id.FacultiesRecView);
         UniversitiesRecView = findViewById(R.id.UniversitiesRecView);
 
 
         ArrayList<University> universities = new ArrayList<>();
         ArrayList<University> faculties = new ArrayList<>();
+        filters = new ArrayList<>();
 
         universities.add(new University("Ryan GOD Gosling", "https://pbs.twimg.com/media/F0mt2ApXwAE7Lmt?format=jpg&name=large"));
         universities.add(new University("Ryan GOD Gosling", "https://pbs.twimg.com/media/F0mt2ApXwAE7Lmt?format=jpg&name=large"));
@@ -80,20 +99,5 @@ public class SearchActivity extends AppCompatActivity {
 //            chip.setId(ViewCompat.generateViewId());
 //            chipGroup.addView(chip);
 //        }
-
-
-        chipGroup.setOnCheckedStateChangeListener(new ChipGroup.OnCheckedStateChangeListener() {
-            @Override
-            public void onCheckedChanged(@NonNull ChipGroup group, @NonNull List<Integer> checkedIds) {
-
-            }
-        });
-    }
-
-    public static void createChip(String text, View view) {
-        Chip chip = (Chip) LayoutInflater.from(view.getContext()).inflate(R.layout.chip_layout, null);
-        chip.setText(text);
-        chip.setId(ViewCompat.generateViewId());
-        chipGroup.addView(chip);
     }
 }
---
+++ app/src/main/java/com/example/internapp/StartActivity.java
@@ -0,0 +1,24 @@
+package com.example.internapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}
