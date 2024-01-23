e220855e43602aaa6d6169cebc61694e9e834bdc
Ruslan <ruslan.yakubov05@gmail.com>
2024-01-23 22:50:15 +0200

    Speed Dial Button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,17 +39,17 @@ android {
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation(platform("com.google.firebase:firebase-bom:32.7.0"))
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-auth:22.3.0")
+    implementation("com.google.firebase:firebase-auth:22.3.1")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
-    implementation("androidx.annotation:annotation:1.6.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.annotation:annotation:1.7.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("com.leinardi.android:speed-dial:3.3.0")
 
-    implementation ("com.github.bumptech.glide:glide:4.16.0")
-
+    implementation("com.github.bumptech.glide:glide:4.16.0")
 }
--- app/src/main/java/com/example/internapp/HomepageActivity.java
+++ app/src/main/java/com/example/internapp/HomepageActivity.java
@@ -2,14 +2,23 @@ package com.example.internapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ActionMenuView;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.leinardi.android.speeddial.SpeedDialActionItem;
+import com.leinardi.android.speeddial.SpeedDialView;
+
+import java.util.ArrayList;
 
 public class HomepageActivity extends AppCompatActivity {
 
@@ -20,6 +29,8 @@ public class HomepageActivity extends AppCompatActivity {
     Button searchBtn;
     Button favoritesBtn;
     TextView welcomeText;
+
+    SpeedDialView speedDialView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +77,61 @@ public class HomepageActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
                 startActivity(intent);
+            }
+        });
+
+
+        speedDialView = findViewById(R.id.speedDialView);
+        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.action_settings, R.drawable.ic_settings)
+                .setFabBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.lightblue, getTheme()))
+                .setFabImageTintColor(ResourcesCompat.getColor(getResources(), R.color.blue, getTheme())).create());
+
+        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.action_home, R.drawable.ic_home)
+                .setFabBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.lightblue, getTheme()))
+                .setFabImageTintColor(ResourcesCompat.getColor(getResources(), R.color.blue, getTheme())).create());
+
+        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.action_favorites, R.drawable.ic_favorites)
+                .setFabBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.lightblue, getTheme()))
+                .setFabImageTintColor(ResourcesCompat.getColor(getResources(), R.color.blue, getTheme())).create());
+
+        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.action_schedule, R.drawable.ic_schedule)
+                .setFabBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.lightblue, getTheme()))
+                .setFabImageTintColor(ResourcesCompat.getColor(getResources(), R.color.blue, getTheme())).create());
+
+        speedDialView.addActionItem(new SpeedDialActionItem.Builder(R.id.action_search, R.drawable.ic_search)
+                .setFabBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.lightblue, getTheme()))
+                .setFabImageTintColor(ResourcesCompat.getColor(getResources(), R.color.blue, getTheme())).create());
+
+
+
+        speedDialView.inflate(R.menu.main_menu);
+        speedDialView.setExpansionMode(SpeedDialView.ExpansionMode.LEFT);
+        speedDialView.setUseReverseAnimationOnClose(true);
+        speedDialView.getMainFab().setCustomSize(200);
+        speedDialView.setOrientation(LinearLayout.VERTICAL);
+//        getMenuInflater().inflate(R.menu.main_menu, ActionMenuView.menu);
+        speedDialView.setOnActionSelectedListener(new SpeedDialView.OnActionSelectedListener() {
+            @Override
+            public boolean onActionSelected(SpeedDialActionItem actionItem) {
+                if (actionItem.getId() == R.id.action_settings) {
+                    Toast.makeText(getApplicationContext(), "You clicked: " + actionItem.getId() + "Label: " + actionItem.getLabel(getApplicationContext()), Toast.LENGTH_SHORT).show();
+                    speedDialView.close(true);
+                } else if (actionItem.getId() == R.id.action_home) {
+                    Toast.makeText(getApplicationContext(), "You're here already!", Toast.LENGTH_SHORT).show();
+                    speedDialView.close(true);
+                } else if (actionItem.getId() == R.id.action_favorites) {
+                    speedDialView.close(true);
+                    Intent intent = new Intent(getApplicationContext(), FavoritesActivity.class);
+                    startActivity(intent);
+                } else if (actionItem.getId() == R.id.action_schedule) {
+                    Toast.makeText(getApplicationContext(), "You clicked: " + actionItem.getId() + "Label: " + actionItem.getLabel(getApplicationContext()), Toast.LENGTH_SHORT).show();
+                    speedDialView.close(true);
+                } else if (actionItem.getId() == R.id.action_search) {
+                    speedDialView.close(true);
+                    Intent intent = new Intent(getApplicationContext(), SearchActivity.class);
+                    startActivity(intent);
+                }
+                return true;
             }
         });
     }
