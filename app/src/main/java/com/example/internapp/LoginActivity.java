1d8a46035ad1f85746dd5f2f9203c1a616360595
Ruslan <ruslan.yakubov05@gmail.com>
2024-01-25 02:00:02 +0200

    Google TODO

--- app/src/main/java/com/example/internapp/LoginActivity.java
+++ app/src/main/java/com/example/internapp/LoginActivity.java
@@ -1,42 +1,50 @@
 package com.example.internapp;
 
+import static android.content.ContentValues.TAG;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.identity.BeginSignInRequest;
+import com.google.android.gms.auth.api.identity.BeginSignInResult;
+import com.google.android.gms.auth.api.identity.Identity;
 import com.google.android.gms.auth.api.identity.SignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.identity.SignInCredential;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-import java.util.Objects;
-
 public class LoginActivity extends AppCompatActivity {
+    private static final int REQ_ONE_TAP = 100;
     FirebaseAuth mAuth;
+    FirebaseUser currentUser;
+    ActivityResultLauncher<Intent> activityResultLauncher;
     private EditText usernameEdt;
     private EditText passwordEdt;
     private Button loginBtn;
-    private Button signUp;
     private Button toRegister;
-    private GoogleSignInClient client;
+    private SignInClient oneTapClient;
+    private BeginSignInRequest signInRequest;
 
     public void onStart() {
         super.onStart();
@@ -53,9 +61,95 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-
         mAuth = FirebaseAuth.getInstance();
+
+        oneTapClient = Identity.getSignInClient(this);
+        signInRequest = BeginSignInRequest.builder()
+                .setPasswordRequestOptions(BeginSignInRequest.PasswordRequestOptions.builder()
+                        .setSupported(true)
+                        .build())
+                .setGoogleIdTokenRequestOptions(BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                        .setSupported(true)
+                        // Your server's client ID, not your Android client ID.
+                        .setServerClientId("1084322055281-rk1so85ivs3qb30tq3ak6pc2ct0d31oh.apps.googleusercontent.com")
+                        // Only show accounts previously used to sign in.
+                        .setFilterByAuthorizedAccounts(false)
+                        .build())
+                // Automatically sign in when exactly one credential is retrieved.
+                .setAutoSelectEnabled(true)
+                .build();
+
+
+        Button buttonGoogleSignIn = findViewById(R.id.googleSignIn);
+
+        activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult o) {
+                        if (o.getResultCode() == Activity.RESULT_OK) {
+                            try {
+                                SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(o.getData());
+                                String idToken = credential.getGoogleIdToken();
+                                String username = credential.getId();
+                                String password = credential.getPassword();
+                                mAuth.signInWithCredential(GoogleAuthProvider.getCredential(credential.getGoogleIdToken(), null))
+                                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                                currentUser = mAuth.getCurrentUser();
+                                                Intent intent = o.getData();
+                                                startActivity(intent);
+                                                finish();
+                                            }
+                                        });
+
+
+                                if (idToken != null) {
+                                    // Got an ID token from Google. Use it to authenticate
+                                    // with your backend.
+                                    Log.d(TAG, "Got ID token.");
+                                } else if (password != null) {
+                                    // Got a saved username and password. Use them to authenticate
+                                    // with your backend.
+                                    Log.d(TAG, "Got password.");
+                                }
+                            } catch (ApiException e) {
+                                Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    }
+                }
+        );
+        buttonGoogleSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                oneTapClient.beginSignIn(signInRequest)
+                        .addOnSuccessListener(new OnSuccessListener<BeginSignInResult>() {
+                            @Override
+                            public void onSuccess(BeginSignInResult result) {
+//                                try {
+                                currentUser = mAuth.getCurrentUser();
+                                activityResultLauncher.launch(new Intent(getApplicationContext(), HomepageActivity.class));
+//                                    startIntentSenderForResult(
+//                                            result.getPendingIntent().getIntentSender(), REQ_ONE_TAP,
+//                                            null, 0, 0, 0);
+//                                } catch (IntentSender.SendIntentException e) {
+//                                    Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+//                                }
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // No saved credentials found. Launch the One Tap sign-up flow, or
+                                // do nothing and continue presenting the signed-out UI.
+                                Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        });
+            }
+        });
+
 
         toRegister = findViewById(R.id.ToRegister);
         loginBtn = findViewById(R.id.loginBtn);
@@ -92,6 +186,7 @@ public class LoginActivity extends AppCompatActivity {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
+                                    currentUser = mAuth.getCurrentUser();
                                     Toast.makeText(getApplicationContext(), "Login successful Brother", Toast.LENGTH_SHORT).show();
                                     Intent intent = new Intent(getApplicationContext(), HomepageActivity.class);
                                     startActivity(intent);
@@ -105,41 +200,43 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
-        Button signUp = findViewById(R.id.googleSignIn);
-        GoogleSignInOptions options = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id)).requestEmail().build();
 
-        client = GoogleSignIn.getClient(this, options);
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = client.getSignInIntent();
-                startActivityForResult(i, 1234);
-            }
-        });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1234) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            try {
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
-                FirebaseAuth.getInstance().signInWithCredential(credential).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Intent intent = new Intent(getApplicationContext(), HomepageActivity.class);
-                            startActivity(intent);
-                        } else {
-                            Toast.makeText(LoginActivity.this, Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-            } catch (ApiException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == REQ_ONE_TAP) {
+//            try {
+//                SignInCredential credential = oneTapClient.getSignInCredentialFromIntent(data);
+//                String idToken = credential.getGoogleIdToken();
+//                String username = credential.getId();
+//                String password = credential.getPassword();
+//                mAuth.signInWithCredential(GoogleAuthProvider.getCredential(credential.getGoogleIdToken(), null))
+//                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                            @Override
+//                            public void onComplete(@NonNull Task<AuthResult> task) {
+//                                FirebaseUser currentUser = mAuth.getCurrentUser();
+//                                Intent intent = new Intent(getApplicationContext(), HomepageActivity.class);
+//                                startActivity(intent);
+//                                finish();
+//                            }
+//                        });
+//
+//
+//                if (idToken != null) {
+//                    // Got an ID token from Google. Use it to authenticate
+//                    // with your backend.
+//                    Log.d(TAG, "Got ID token.");
+//                } else if (password != null) {
+//                    // Got a saved username and password. Use them to authenticate
+//                    // with your backend.
+//                    Log.d(TAG, "Got password.");
+//                }
+//            } catch (ApiException e) {
+//                Toast.makeText(getApplicationContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//    }
 }
